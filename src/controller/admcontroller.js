2e9a3c87fa68e8ae37f31c348dccb3e3833ddb55
adrianjame <adrianjamesbadwolf@gmail.com>
2023-09-17 15:40:48 -0300

    loginadm

---
+++ src/controller/admcontroller.js
@@ -0,0 +1,20 @@
+import { LoginAdm } from "../repository/admrepository.js";
+
+
+const endpoints = Router();
+
+endpoints.post('/adm/login', async (req, resp) => {
+    try{
+        let adm = req.body
+        let r = await LoginAdm(adm)
+        resp.send(r)
+    }
+
+    catch (err){
+        resp.status(500).send({ erro: 'Ocorreu um erro!'})
+    };
+})
+
+
+
+export default endpoints;
